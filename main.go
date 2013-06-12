a4d70d316ec545d0f706d932cc753976e344f5f1
Ian Coleman <ian.coleman@sitepoint.com>
2013-06-12 14:52:56 +1000

    Url for further inspiration added as comment

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ func yellow(text string) string {
     return color(text, 33)
 }
 
+// see http://www.darkcoding.net/software/pretty-command-line-console-output-on-unix-in-python-and-go-lang/
 func color(text string, color int) string {
     return fmt.Sprintf("\033[%dm%v\033[0m", color, text)
 }
